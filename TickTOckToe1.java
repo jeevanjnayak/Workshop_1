f9090557b985d73fbb7e64abb86639677894fb7d
jeevan <jeevanjnayak@gmail.com>
2022-01-28 09:00:38 +0530

    initialised: initialised the array

---
+++ TickTOckToe1.java
@@ -0,0 +1,2 @@
+package com.company.Workshop_1;public class TickTOckToe1 {
+}
---
+++ TickTocToe.java
@@ -0,0 +1,23 @@
+package com.Bridgelabz;
+
+
+public class TickTocToe {
+
+    public static String start()
+    {
+        char[] Arr = new char[10];
+        for (int i = 1; i < 10; i++) {
+            Arr[i] = ' ';
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        String strt = start();
+        System.out.println(strt);
+
+
+
+    }
+}
