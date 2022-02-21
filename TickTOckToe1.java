ba5165917f86e684e153127291bf01428f456ca7
Jeevanjyoti nayak <60505179+jeevanjnayak@users.noreply.github.com>
2022-02-21 17:35:34 +0530

    Update TickTOckToe1.java

--- TickTOckToe1.java
+++ TickTOckToe1.java
@@ -1,2 +1,23 @@
-package com.company.Workshop_1;public class TickTOckToe1 {
+package com.Bridgelabz;
+
+
+public class TickTocToe {
+
+    public static String start()
+    {
+        char[] Arr = new char[10];
+        for (int i = 1; i < 10; i++) {
+            Arr[i] = ' ';
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        String strt = start();
+        System.out.println(strt);
+
+
+
+    }
 }
